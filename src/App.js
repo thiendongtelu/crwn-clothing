9fc4f8be62203e7f59680a8462b84283064a0818
thiendongtelu <59526587+thiendongtelu@users.noreply.github.com>
2020-01-19 09:01:37 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
